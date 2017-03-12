15058e3b0e1c5c4d93c2b7537e12e09b708a1e1b
Andy Liu <enhengliu@gmail.com>
2017-03-12 09:30:29 +0800

    Initial Commit

---
+++ TaipeiPark/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaipeiPark
+//
+//  Created by Andy Liu on 2017/3/12.
+//  Copyright © 2017年 Andy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
